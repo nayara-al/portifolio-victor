3a8d9e68b73c8021642de93a10dc9293e6d17603
nayara-al <anna_nayara@hotmail.com>
2023-07-18 17:02:52 -0300

    criação do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "azul-claro": '#1875E8',
+        "azul-escuro": '#041833',
+        branco: '#F0F0F0',
+        cinza: '#444444',
+        'verde-android': '#A4C639',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
