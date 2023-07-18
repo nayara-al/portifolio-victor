4773a53424a51197997586850735c890f917f82f
nayara-al <anna_nayara@hotmail.com>
2023-07-18 17:03:42 -0300

    criação do home

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,7 @@
+export const Banner = () => {
+  return (
+    <div className="w-full px-8 py-4 bg-azul-escuro">
+      <h1 className="text-white font-extrabold font-">Olá, sou Victor</h1>
+    </div>
+  );
+};
