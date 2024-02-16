31159a8d86bcaf6814f61bddb0fb576d13482112
Anna Nayara Lira <anna_nayara@hotmail.com>
2024-02-16 16:19:16 -0300

    refactor: ajuste no menu responisvo

--- src/components/common/Header/NavBarMobile/index.tsx
+++ src/components/common/Header/NavBarMobile/index.tsx
@@ -1,15 +1,19 @@
 "use client";
+
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
+
 export default function NavBarMobile() {
   const [isShow, setIsShow] = useState<boolean>(false);
 
   const changeVisibility = () => {
     setIsShow(!isShow);
   };
+
   return (
-    <div className="md:hidder">
-      <button onClick={changeVisibility}>
+    <div className="md:hidden w-full relative flex justify-end items-center">
+      <button onClick={changeVisibility} className="mr-4">
         {isShow ? (
           <Image
             src="/icons/x-close.svg"
@@ -26,7 +30,24 @@ export default function NavBarMobile() {
           />
         )}
       </button>
-      <div className="w-screen h-screen bg-white"></div>
+      <ul
+        className={`w-screen h-screen flex flex-col justify-center items-center gap-6 text-verde-android py-8 top-12 right-0 z-10 bg-cinza absolute text-xl font-bold ${
+          isShow ? "block" : "hidden"
+        }`}
+      >
+        <Link href="/">
+          <li>Início</li>
+        </Link>
+        <Link href={"/sobre-mim"}>
+          <li>Sobre mim</li>
+        </Link>
+        <Link href={"/projetos"}>
+          <li>Projetos</li>
+        </Link>
+        <Link href={"/cursos"}>
+          <li>Cursos</li>
+        </Link>
+      </ul>
     </div>
   );
 }
